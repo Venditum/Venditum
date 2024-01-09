9964fdb3c7be091bd034c6f5e59ece0b0e2eedf1
Venditum <98831274+Venditum@users.noreply.github.com>
2024-01-09 19:04:44 +0100

    dsf

--- Farning/Python/KI/Blumen.py
+++ Farning/Python/KI/Blumen.py
@@ -1,42 +1,46 @@
 import csv
 import math
 
-def euklidischer_abstand(p, q):
+def euklidischer_abstand(zeile1, zeile2):
     a = 0
-    for i in range(len(q)):
-        a += (float(p[i]) - float(q[i]))**2
+    for i in range(len(zeile2)):
+        a += (zeile1[i] - zeile2[i])**2
     return math.sqrt(a)
+
+def normalisieren(liste):
+    maxi = max(liste)
+    for i in range(len(liste)):
+        i /= maxi
 
 datensatz = []
 with open("iris.csv") as f:
-    datensatz = list(csv.reader(f))
+    csvreader = list(csv.reader(f))
+    for zeile in csvreader:
+        datensatz.append([float(wert) for wert in zeile[:-1]] + [zeile[-1]])
 
-test_datensatz = datensatz[0]
+datensatz_transponiert = list(zip(*datensatz))
+maxima = []
+for zeile in datensatz_transponiert:
+    maxima.append(max(zeile))
+for i in range(len(datensatz)):
+    for j in range(len(datensatz[i]) - 2):
+        datensatz[i][j] /= maxima[j]    
 
-trainings_datensatz = datensatz[1:]
+def predict(datensatz, probant, abstandvalue):
+    distances_datensatz = []
+    for zeile in datensatz:
+        distances_datensatz.append((euklidischer_abstand(probant, zeile[:-1]), zeile[-1]))
 
-name_list = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+    top_k = list(filter(lambda x: x[0] <= abstandvalue, distances_datensatz))    
+    prediction = max([abstand[-1] for abstand in top_k], key= top_k.count)
 
-k = 10
+    return prediction
 
-distances_datensatz = []
-for zeile in trainings_datensatz:
-    distances_datensatz.append((euklidischer_abstand(test_datensatz, zeile[:-1]), zeile[-1]))
+def evaluate(abstandvalue):
+    korrekt = 0
+    for i in datensatz:
+        if predict(datensatz, i, abstandvalue) == i[-1]:
+            korrekt += 1
+    return korrekt / len(datensatz)
 
-distances_datensatz.sort(key=lambda x : x[0])
-top_k = distances_datensatz[:k]
-
-prediction = max([abstand[-1] for abstand in top_k], key= top_k.count)
-
-print(prediction)
-
-def normalisieren_datensatz(liste):
-    maxwerte = []
-    for i in liste:
-        maxwerte.append(max(liste))
-    maxi = max(maxwerte) 
-    for i in liste:
-        for x in i:
-            x /= maxi  
-
-normalisieren_datensatz(distances_datensatz)
+print(evaluate(0.05))
--- Farning/Python/KI/BlumenRadius.py
+++ Farning/Python/KI/BlumenRadius.py
@@ -6,27 +6,26 @@ def euklidischer_abstand(p, q):
     for i in range(len(q)):
         a += (float(p[i]) - float(q[i]))**2
     return math.sqrt(a)
-
 datensatz = []
 with open("iris.csv") as f:
     datensatz = list(csv.reader(f))
+max_abstand = 2   
 
 test_datensatz = datensatz[0]
 
 trainings_datensatz = datensatz[1:]
-
-name_list = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
-
-max_abstand = 2
-
-top_k = []
-
 distances_datensatz = []
 for zeile in trainings_datensatz:
-    distances_datensatz.append((euklidischer_abstand(test_datensatz, zeile[:-1]), zeile[-1]))
+    distances_datensatz.append((euklidischer_abstand(test_datensatz, zeile[:-1]), zeile[-1]))   
 
-distances_datensatz.sort(key=lambda x : x[0])
-top_k = list(filter(lambda x: x[0] <= max_abstand, distances_datensatz))    
+for i in range(10000):
+    top_k = []
 
-prediction = max([abstand[-1] for abstand in top_k], key= top_k.count)
-print(prediction)
+    top_k = list(filter(lambda x: x[0] <= max_abstand, distances_datensatz))    
+
+    prediction = max([abstand[-1] for abstand in top_k], key= top_k.count)
+    if prediction == test_datensatz[-1] and max_abstand >= 0.5:
+        max_abstand += 0.01
+    else:
+        max_abstand -= 0.01  
+print(max_abstand)         
