36f4703dc8342e8fe86bd1a2441bb8fc61aac016
unknown <oscar.boyu.lin@gmail.com>
2024-04-07 21:58:26 +0200

    g

--- Farning/Python/KI/NBK.py
+++ Farning/Python/KI/NBK.py
@@ -1,4 +1,4 @@
-import csv, matplotlib.pyplot as plt, math, multiprocessing, numpy as np
+import csv, matplotlib.pyplot as plt, math, multiprocessing, numpy as np, random, sys
 from matplotlib import cm
 
 def gauss(x, m, s):
@@ -7,8 +7,8 @@ def gauss(x, m, s):
 def m(liste):
     return sum(liste) / len(liste)
 
-def s(datensatz):
-    return math.sqrt((sum([(x - m(datensatz)) ** 2 for x in datensatz])) / len(datensatz))
+def s(liste):
+    return math.sqrt((sum([(x - m(liste)) ** 2 for x in liste])) / len(liste))
 
 datensatz = []
 with open("iris.csv") as f:
@@ -16,45 +16,50 @@ with open("iris.csv") as f:
     for zeile in csv_reader:
         datensatz.append([float(wert) for wert in zeile[:-1]] + [zeile[-1]])
 
-arten = {}
-for i in range(len(datensatz)):
-    if not datensatz[i][-1] in arten:
-        arten[datensatz[i][-1]] = []       
-    arten[datensatz[i][-1]].append(datensatz[i])
-        
-arten_ms = {}
-
-for art in arten:
-    arten_ms[art] = []
-    for i in range(4):
-        arten_ms[art].append((m(list(zip(*arten[art]))[i]), s(list(zip(*arten[art]))[i])))
-
-def predict(blume, gewichte):
+def predict(testdatenzeile, arten_ms, gewichte):
     best = ""
     bestscore = 0
+
     for art in arten_ms: 
         totalscore = 0
-        for i in range(len(blume)):
-            totalscore += gauss(blume[i], arten_ms[art][i][0], arten_ms[art][i][1]) * gewichte[i]  
+        for i in range(len(testdatenzeile)):
+            totalscore += gauss(testdatenzeile[i], arten_ms[art][i][0], arten_ms[art][i][1]) * gewichte[i]  
         if totalscore > bestscore:
             bestscore = totalscore
             best = art 
     return best
 
-def validierung(gewichte):
+def validierung(durchläufe, gewichte, p):
     treffer = 0
-    
-    for i in range(len(datensatz)):
-        vermutung = predict(datensatz[i][:-1], gewichte)
-        if vermutung == datensatz[i][-1]:
-            treffer += 1
+    for _ in range(durchläufe):
+        random.shuffle(datensatz)
+        testdaten = datensatz[:int(len(datensatz) * p)]
+        trainingsdaten = datensatz[int(len(datensatz) * p):]
+        arten = {}
 
-    return treffer / len(datensatz)
+        for i in range(len(trainingsdaten)):
+            if not trainingsdaten[i][-1] in arten:
+                arten[trainingsdaten[i][-1]] = []       
+            arten[trainingsdaten[i][-1]].append(trainingsdaten[i])
+            
+        arten_ms = {}
+
+        for art in arten:
+            arten_ms[art] = []
+            for i in range(4):
+                arten_ms[art].append((m(list(zip(*arten[art]))[i]), s(list(zip(*arten[art]))[i])))
+    
+        for zeile in testdaten:
+            vermutung = predict(zeile[:-1], arten_ms, gewichte)
+            if vermutung == zeile[-1]:
+                treffer += 1
+
+    return treffer / (len(testdaten) * durchläufe)
 
 def gewichte_optimierung(gewichte_3_4):
     d_s = []
     for g_3, g_4 in gewichte_3_4:
-        d = validierung([1, 1, g_3, g_4])
+        d = validierung(12, [1, 1, g_3, g_4], 0.2)
         d_s.append((d, g_3, g_4))
     return max(d_s), d_s
 
@@ -76,7 +81,7 @@ def gewichte_optimierung_multithreaded(gewichte_3, gewichte_4, t):
 
 if __name__ == '__main__':
     anzahl_t = 8
-    d = gewichte_optimierung_multithreaded([i / 100 for i in range(1100, 1400)], [i / 100 for i in range(800, 1100)], anzahl_t)
+    d = gewichte_optimierung_multithreaded([i / 4 for i in range(1, 200)], [i / 4 for i in range(1, 200)], anzahl_t)
     print(f"Beste Genauigkeit: {d[0][0]} bei g_3={d[0][1]} und g_4={d[0][2]}")
     fig = plt.figure("Genauigkeit")
     ax = fig.add_subplot(projection="3d")
